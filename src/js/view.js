c38ee18257231ef21076f6a2167e0b3f6cf4c983
Fatkhi <takova.mail.HET@gmail.com>
2017-10-18 14:18:44 +0300

    MVP

--- src/js/view.js
+++ src/js/view.js
@@ -7,11 +7,17 @@ export default class View {
 		this.model = new Model();
 		this.player1 = qs('#player1');
 		this.player2 = qs('#player2');
-		this.scoreboard = qs('#scoreboard');
+		this.scoreboard = qs('.scoreboard-text');
+		this.simulateButton = qs('.js-simulate');
+		this.playAgainButton = qs('.js-play-again');
 
 		this.player1Moves = [];
 		this.player2Moves = [];
 
+		this.bindListeners();
+	}
+
+	bindListeners() {
 		MOVES.forEach((move) => {
 			const el1 = qs('.js-' + move.toLowerCase(), this.player1);
 			const el2 = qs('.js-' + move.toLowerCase(), this.player2);
@@ -20,9 +26,25 @@ export default class View {
 			this.player2Moves.push(el2);
 
 			el1.addEventListener('click', () => {
-				this.addActiveClass(move, this.player1Moves);
 				this.processMove(move);
 			});
+		});
+
+		this.simulateButton.addEventListener('click', () => {
+
+			this.model.player1.randomizeMove();
+			this.model.player2.randomizeMove();
+			this.model.findWinner();
+
+			this.addActiveClass(this.model.player1.move, this.player1Moves);
+			this.addActiveClass(this.model.player2.move, this.player2Moves);
+
+			this.scoreboard.innerText = this.model.result;
+			this.playAgainButton.classList.remove('hidden');
+		});
+
+		this.playAgainButton.addEventListener('click', () => {
+			this.resetView();
 		});
 	}
 
@@ -30,12 +52,31 @@ export default class View {
 		moveNodesArr[MOVES.indexOf(move)].classList.add('active');
 	}
 
+	resetView() {
+		this.player1Moves.forEach((node) => {
+			node.classList.remove('active');
+		});
+
+		this.player2Moves.forEach((node) => {
+			node.classList.remove('active');
+		});
+
+		this.playAgainButton.classList.add('hidden');
+		this.scoreboard.innerText = 'Make a move';
+	}
+
 	//TODO decouple logic, add observer, render logic in view, better styling, responsive layout
 	processMove(move) {
 		this.model.player1.setMove(move);
 		this.model.player2.randomizeMove();
-		this.model.findWinner();
+
+		this.addActiveClass(this.model.player1.move, this.player1Moves);
 		this.addActiveClass(this.model.player2.move, this.player2Moves);
+
+		this.model.findWinner();
 		this.scoreboard.innerText = this.model.result;
+		this.playAgainButton.classList.remove('hidden');
 	}
+
+
 }
